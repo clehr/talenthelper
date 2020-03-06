6d9e30d68a69890d210590f0d74b4a67e0bddc56
clehr <christian_lehr@outlook.de>
2020-03-06 08:49:00 +0100

    align button in the middle

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
   const backgroundUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=Monk&spec=Mistweaver&region=2";
   return (
     <div className="app">
+
       <button>
         <a href={backgroundUrl} target="_blank" rel="noopener noreferrer">
           show me my spec!
         </a>
       </button>
+
     </div>
   );
 }
