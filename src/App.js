435dc5a57472d1971df2a4d2d2dac456e4daa6d8
clehr <christian_lehr@outlook.de>
2020-03-06 08:41:16 +0100

    extract image url as variable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const backgroundUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=Monk&spec=Mistweaver&region=2";
   return (
     <div className="app">
       <button>
-        <a href="https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=Monk&spec=Mistweaver&region=2" target="_blank">
+        <a href={backgroundUrl} target="_blank" rel="noopener noreferrer">
           show me my spec!
         </a>
       </button>
