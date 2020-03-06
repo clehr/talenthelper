3b0a8bfb3f66da6a6a07765ebb23e6a75c8e2cc4
clehr <christian_lehr@outlook.de>
2020-03-06 09:02:24 +0100

    add dropdowns

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ function App() {
   return (
     <div className="app">
 
+      <label for="class">Your class:</label>
+      <select id="class">
+        <option value="Monk">Monk</option>
+        <option value="Warlock">Warlock</option>
+        <option value="Warrior">Warrior</option>
+        <option value="Mage">Mage</option>
+      </select>
+
+      <label for="spec">Your spec:</label>
+      <select id="spec">
+        <option value="Mistweaver">Mistweaver</option>
+        <option value="Brewmaster">Brewmaster</option>
+        <option value="Windwalker">Windwalker</option>
+      </select>
+
       <button>
         <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
           show me my spec!
