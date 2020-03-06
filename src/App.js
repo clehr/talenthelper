4071e8bc89f13b6299f4e5ad580a3289b343767b
clehr <christian_lehr@outlook.de>
2020-03-06 08:26:02 +0100

    add button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div>
-
+      <button>
+        <a href="https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=Monk&spec=Mistweaver&region=2" target="_blank">
+          show me my spec!
+        </a>
+      </button>
     </div>
   );
 }
