ccbf3a937c84d09aae53ca5e171e61257d492a2f
clehr <christian_lehr@outlook.de>
2020-03-06 08:57:10 +0100

    extract variable for url

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const backgroundUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=Monk&spec=Mistweaver&region=2";
+  const spec = 'Mistweaver';
+  const wowClass = 'Monk';
+  const dungeonUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=" + wowClass + "&spec=" + spec + "&region=2";
   return (
     <div className="app">
 
       <button>
-        <a href={backgroundUrl} target="_blank" rel="noopener noreferrer">
+        <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
           show me my spec!
         </a>
       </button>
