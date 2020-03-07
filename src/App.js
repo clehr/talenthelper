95432e560dddae901d2972c0a6703afca93a9c02
clehr <christian_lehr@outlook.de>
2020-03-07 09:17:57 +0100

    add divs to components

--- src/App.js
+++ src/App.js
@@ -8,38 +8,49 @@ function App() {
   return (
     <div className="app">
 
-      <img className="logo" src="https://vignette.wikia.nocookie.net/wowwiki/images/5/5e/New_WorldOfWarcraft_logo_large.png/revision/latest?cb=20131220175833" />
+      <div>
+        <img className="logo" src="https://vignette.wikia.nocookie.net/wowwiki/images/5/5e/New_WorldOfWarcraft_logo_large.png/revision/latest?cb=20131220175833" alt="logo" />
+      </div>
 
-      <h3>Hey there!</h3>
+      <div>
+        <h3>Hey there!</h3>
+      </div>
 
-      <label for="usecase">What do you want to do?:</label>
-      <select id="usecase">
-        <option value="Mythic">Mythic</option>
-        <option value="Raid">Raid</option>
-        <option value="PvP">Warrior</option>
-      </select>
+      <div>
+        <label for="usecase">What do you want to do?:</label>
+        <select id="usecase">
+          <option value="Mythic">Mythic</option>
+          <option value="Raid">Raid</option>
+          <option value="PvP">Warrior</option>
+        </select>
+      </div>
 
-      <label for="class">Your class:</label>
-      <select id="class">
-        <option value="Monk">Monk</option>
-        <option value="Warlock">Warlock</option>
-        <option value="Warrior">Warrior</option>
-        <option value="Mage">Mage</option>
-      </select>
+      <div>
+        <label for="class">Your class:</label>
+        <select id="class">
+          <option value="Monk">Monk</option>
+          <option value="Warlock">Warlock</option>
+          <option value="Warrior">Warrior</option>
+          <option value="Mage">Mage</option>
+        </select>
+      </div>
 
-      <label for="spec">Your spec:</label>
-      <select id="spec">
-        <option value="Mistweaver">Mistweaver</option>
-        <option value="Brewmaster">Brewmaster</option>
-        <option value="Windwalker">Windwalker</option>
-      </select>
+      <div>
+        <label for="spec">Your spec:</label>
+        <select id="spec">
+          <option value="Mistweaver">Mistweaver</option>
+          <option value="Brewmaster">Brewmaster</option>
+          <option value="Windwalker">Windwalker</option>
+        </select>
+      </div>
 
-      <button>
-        <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
-          Tell me how to skill!
+      <div>
+        <button>
+          <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
+            Tell me how to skill!
         </a>
-      </button>
-
+        </button>
+      </div>
     </div>
   );
 }
