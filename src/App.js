e158d43ff79b5385e22e1522354ab11e16fcb082
clehr <christian_lehr@outlook.de>
2020-03-07 09:36:38 +0100

    refactor to class

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  const spec = 'Mistweaver';
-  const wowClass = 'Monk';
-  const dungeonUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=" + wowClass + "&spec=" + spec + "&region=2";
-  return (
-    <div>
+export default class App extends React.Component {
+
+  render() {
+    const spec = 'Mistweaver';
+    const wowClass = 'Monk';
+    const dungeonUrl = "https://www.warcraftlogs.com/zone/rankings/20#metric=playerscore&class=" + wowClass + "&spec=" + spec + "&region=2";
+
+    return (
       <div>
-        <img src="https://vignette.wikia.nocookie.net/wowwiki/images/5/5e/New_WorldOfWarcraft_logo_large.png/revision/latest?cb=20131220175833" alt="logo" />
+        <div>
+          <img src="https://vignette.wikia.nocookie.net/wowwiki/images/5/5e/New_WorldOfWarcraft_logo_large.png/revision/latest?cb=20131220175833" alt="logo" />
+        </div>
+        <div className="black-section">
+          <div>
+            <h3>Hey there!</h3>
+          </div>
+
+          <div>
+            <label htmlFor="usecase">What do you want to do?:</label>
+            <select id="usecase">
+              <option value="Mythic">Mythic</option>
+              <option value="Raid">Raid</option>
+              <option value="PvP">Warrior</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="class">Your class:</label>
+            <select id="class">
+              <option value="Monk">Monk</option>
+              <option value="Warlock">Warlock</option>
+              <option value="Warrior">Warrior</option>
+              <option value="Mage">Mage</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="spec">Your spec:</label>
+            <select id="spec">
+              <option value="Mistweaver">Mistweaver</option>
+              <option value="Brewmaster">Brewmaster</option>
+              <option value="Windwalker">Windwalker</option>
+            </select>
+          </div>
+
+          <div>
+            <button>
+              <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
+                Tell me how to skill!
+          </a>
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="black-section">
-        <div>
-          <h3>Hey there!</h3>
-        </div>
-
-        <div>
-          <label htmlFor="usecase">What do you want to do?:</label>
-          <select id="usecase">
-            <option value="Mythic">Mythic</option>
-            <option value="Raid">Raid</option>
-            <option value="PvP">Warrior</option>
-          </select>
-        </div>
-
-        <div>
-          <label htmlFor="class">Your class:</label>
-          <select id="class">
-            <option value="Monk">Monk</option>
-            <option value="Warlock">Warlock</option>
-            <option value="Warrior">Warrior</option>
-            <option value="Mage">Mage</option>
-          </select>
-        </div>
-
-        <div>
-          <label htmlFor="spec">Your spec:</label>
-          <select id="spec">
-            <option value="Mistweaver">Mistweaver</option>
-            <option value="Brewmaster">Brewmaster</option>
-            <option value="Windwalker">Windwalker</option>
-          </select>
-        </div>
-
-        <div>
-          <button>
-            <a href={dungeonUrl} target="_blank" rel="noopener noreferrer">
-              Tell me how to skill!
-        </a>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+
