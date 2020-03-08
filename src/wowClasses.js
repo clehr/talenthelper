2ae495ac8afe3038566201155b7949473e9c1fb8
clehr <christian_lehr@outlook.de>
2020-03-08 13:57:47 +0100

    fix demon hunter and dk links

--- src/wowClasses.js
+++ src/wowClasses.js
@@ -4,11 +4,11 @@ export const classNames = [
     { name: "Warlock", specs: ["Destruction", "Affliction", "Demonology"] },
     { name: "Mage", specs: ["Arcane", "Fire", "Frost"] },
     { name: "Paladin", specs: ["Holy", "Retribution", "Protection"] },
-    { name: "Death Knight", specs: ["Unholy", "Frost", "Blood"] },
+    { name: "DeathKnight", specs: ["Unholy", "Frost", "Blood"] },
     { name: "Rogue", specs: ["Subtlety", "Assasination", "Outlaw"] },
     { name: "Hunter", specs: ["Beast Mastery", "Survival", "Marksmanship"] },
     { name: "Priest", specs: ["Shadow", "Holy", "Disciplin", "Healing"] },
     { name: "Druid", specs: ["Feral", "Guardian", "Restoration", "Balance"] },
     { name: "Shaman", specs: ["Restoration", "Elemental", "Enhancement"] },
-    { name: "Demon Hunter", specs: ["Vengeance", "Havoc"] }
+    { name: "DemonHunter", specs: ["Vengeance", "Havoc"] }
 ];
